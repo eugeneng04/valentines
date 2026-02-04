249f3a7e7b1a3a1b03759c65fd969f140823f11c
Eugene Ng <eugene1862@gmail.com>
2026-02-03 19:52:56 -0800

    Add GitHub Actions deploy workflow and fix router basename

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename="/valentines">
         <Routes>
           <Route path="/" element={<Index />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
